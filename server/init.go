dc448632788d9fbea05eac714dd5c050517b5fae
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-07-19 14:25:37 +0200

    feat(desmos): added GenerateSaveCoinKeyFromPath method

--- server/init.go
+++ server/init.go
@@ -10,32 +10,23 @@ import (
 // Deprecated: GenerateCoinKey generates a new key mnemonic along with its addrress.
 // Please use testutils.GenerateCoinKey instead.
 func GenerateCoinKey(algo keyring.SignatureAlgo) (sdk.AccAddress, string, error) {
-	// generate a private key, with mnemonic
-	info, secret, err := keyring.NewInMemory().NewMnemonic(
-		"name",
-		keyring.English,
-		sdk.GetConfig().GetFullBIP44Path(),
-		keyring.DefaultBIP39Passphrase,
-		algo,
-	)
+	// generate a private key, with recovery phrase
+	info, secret, err := keyring.NewInMemory().NewMnemonic("name", keyring.English, sdk.FullFundraiserPath, keyring.DefaultBIP39Passphrase, algo)
 	if err != nil {
-		return sdk.AccAddress{}, "", err
+		return sdk.AccAddress([]byte{}), "", err
 	}
-
 	return sdk.AccAddress(info.GetPubKey().Address()), secret, nil
 }
 
-// Deprecated: GenerateSaveCoinKey generates a new key mnemonic with its addrress.
-// If mnemonic is provided then it's used for key generation.
-// The key is saved in the keyring. The function returns error if overwrite=true and the key
-// already exists.
-// Please use testutils.GenerateSaveCoinKey instead.
-func GenerateSaveCoinKey(
-	keybase keyring.Keyring,
-	keyName string,
-	overwrite bool,
-	algo keyring.SignatureAlgo,
-) (sdk.AccAddress, string, error) {
+// GenerateSaveCoinKey returns the address of a public key, along with the secret
+// phrase to recover the private key.
+func GenerateSaveCoinKey(keybase keyring.Keyring, keyName string, overwrite bool, algo keyring.SignatureAlgo) (sdk.AccAddress, string, error) {
+	return GenerateSaveCoinKeyFromPath(keybase, keyName, overwrite, algo, sdk.FullFundraiserPath)
+}
+
+// GenerateSaveCoinKeyFromPath returns the address of a public key, along with the secret
+// phrase to recover the private key.
+func GenerateSaveCoinKeyFromPath(keybase keyring.Keyring, keyName string, overwrite bool, algo keyring.SignatureAlgo, path string) (sdk.AccAddress, string, error) {
 	exists := false
 	_, err := keybase.Key(keyName)
 	if err == nil {
@@ -54,10 +45,10 @@ func GenerateSaveCoinKey(
 		}
 	}
 
-	k, mnemonic, err := keybase.NewMnemonic(keyName, keyring.English, sdk.GetConfig().GetFullBIP44Path(), keyring.DefaultBIP39Passphrase, algo)
+	info, secret, err := keybase.NewMnemonic(keyName, keyring.English, path, keyring.DefaultBIP39Passphrase, algo)
 	if err != nil {
 		return sdk.AccAddress{}, "", err
 	}
 
-	return sdk.AccAddress(k.GetAddress()), mnemonic, nil
+	return sdk.AccAddress(info.GetPubKey().Address()), secret, nil
 }
